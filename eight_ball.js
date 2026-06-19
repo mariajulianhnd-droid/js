446401130f660d2d7719332ddc8d7b7d782d8d35
mariajulianhnd-droid <mariajulianhnd@gmai.com>
2026-06-19 15:44:08 +0700

    first commit

---
+++ eight_ball.js
@@ -0,0 +1,48 @@
+let userName = 'Erdin';
+
+userName === 'Erdin' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'Lu cewek bukan';
+console.log(`${userName} asked : \'${userQuestion}?\'`);
+
+let randomNumber = Math.floor(Math.random() * 8) + 1;
+
+eightBall = randomNumber;
+//console.log (`The number is ${eightBall}`);
+switch (eightBall) {
+  case 1 : 
+  console.log('It is certain');
+  break;
+
+  case 2 : 
+  console.log('It is decidedly so');
+  break;
+
+  case 3 : 
+  console.log('Reply hazy try again');
+  break;  
+
+  case 4 : 
+  console.log('Cannot predict now');
+  break;
+
+  case 5 : 
+  console.log('Do not count on it');
+  break;
+
+  case 6 : 
+  console.log('My sources say no');
+  break;
+  
+  case 7 : 
+  console.log('Outlook not so good');
+  break;
+
+  case 8 : 
+  console.log('Signs point to yes');
+  break;
+
+  default : 
+  console.log('Something went wrong!');
+  break;
+}
